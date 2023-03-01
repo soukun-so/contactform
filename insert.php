b961d4eba884d2fe3d82d2d060da18981c6ea4b1
sasaso <sido0903@gmail.com>
2023-03-01 22:13:54 +0900

    Lesson_contactform

---
+++ insert.php
@@ -0,0 +1,24 @@
+<?php
+mb_internal_encoding("utf8");
+$pdo = new PDO("mysql:dbname=lesson01;host=localhost;","root","sota1006");
+
+$pdo->exec("
+    insert into contactform(name,mail,age,comments)
+    values('".$_POST['name']."','".$_POST['mail']."','".$_POST['age']."','".$_POST['comments']."');
+    ");
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <metacharset="utf-8">
+        <title>お問い合わせフォームを作ろう</title>
+        <link rel="stylesheet" type="text/css" href="style2.css">
+    </head>
+    <body>
+        <h1>お問い合わせフォーム</h1>
+        <div class="confirm">
+            <p>お問い合わせ有難うございました。<br>3営業日以内に担当者よりご連絡差し上げます。</p>
+        </div>
+    </body>
+</html>
